a18ba30261c75e6537f107054a92f9ac32e4a8f7
浅墨 <happylifemxy@163.com>
2016-10-12 16:18:46 +0800

    add Interpreter  and Iterator Pattern

---
+++ Assets/Interpreter Pattern/Structure/InterpreterStructrue.cs
@@ -0,0 +1,67 @@
+﻿//-------------------------------------------------------------------------------------
+//	InterpreterStructrue.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class InterpreterStructrue : MonoBehaviour
+{
+	void Start ( )
+    {
+        Context context = new Context();
+
+        // Usually a tree 
+        ArrayList list = new ArrayList();
+
+        // Populate 'abstract syntax tree' 
+        list.Add(new TerminalExpression());
+        list.Add(new NonterminalExpression());
+        list.Add(new TerminalExpression());
+        list.Add(new TerminalExpression());
+
+        // Interpret
+        foreach (AbstractExpression exp in list)
+        {
+            exp.Interpret(context);
+        }
+
+    }
+}
+
+/// <summary>
+/// The 'Context' class
+/// </summary>
+class Context
+{
+}
+
+/// <summary>
+/// The 'AbstractExpression' abstract class
+/// </summary>
+abstract class AbstractExpression
+{
+    public abstract void Interpret(Context context);
+}
+
+/// <summary>
+/// The 'TerminalExpression' class
+/// </summary>
+class TerminalExpression : AbstractExpression
+{
+    public override void Interpret(Context context)
+    {
+        Debug.Log("Called Terminal.Interpret()");
+    }
+}
+
+/// <summary>
+/// The 'NonterminalExpression' class
+/// </summary>
+class NonterminalExpression : AbstractExpression
+{
+    public override void Interpret(Context context)
+    {
+        Debug.Log("Called Nonterminal.Interpret()");
+    }
+}
---
+++ Assets/Iterator Pattern/Structure/IteratorStructure.cs
@@ -0,0 +1,120 @@
+﻿//-------------------------------------------------------------------------------------
+//	IteratorStructure.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class IteratorStructure : MonoBehaviour
+{
+	void Start ( )
+	{
+        ConcreteAggregate a = new ConcreteAggregate();
+        a[0] = "Item A";
+        a[1] = "Item B";
+        a[2] = "Item C";
+        a[3] = "Item D";
+
+        // Create Iterator and provide aggregate
+        Iterator i = a.CreateIterator();
+
+        Debug.Log("Iterating over collection:");
+
+        object item = i.First();
+        while (item != null)
+        {
+            Debug.Log(item);
+            item = i.Next();
+        }
+    }
+}
+
+    /// <summary>
+    /// The 'Aggregate' abstract class
+    /// </summary>
+    abstract class Aggregate
+    {
+        public abstract Iterator CreateIterator();
+    }
+
+    /// <summary>
+    /// The 'ConcreteAggregate' class
+    /// </summary>
+    class ConcreteAggregate : Aggregate
+    {
+        private ArrayList _items = new ArrayList();
+
+        public override Iterator CreateIterator()
+        {
+            return new ConcreteIterator(this);
+        }
+
+        // Gets item count
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+
+        // Indexer
+        public object this[int index]
+        {
+            get { return _items[index]; }
+            set { _items.Insert(index, value); }
+        }
+    }
+
+    /// <summary>
+    /// The 'Iterator' abstract class
+    /// </summary>
+    abstract class Iterator
+    {
+        public abstract object First();
+        public abstract object Next();
+        public abstract bool IsDone();
+        public abstract object CurrentItem();
+    }
+
+    /// <summary>
+    /// The 'ConcreteIterator' class
+    /// </summary>
+    class ConcreteIterator : Iterator
+    {
+        private ConcreteAggregate _aggregate;
+        private int _current = 0;
+
+        // Constructor
+        public ConcreteIterator(ConcreteAggregate aggregate)
+        {
+            this._aggregate = aggregate;
+        }
+
+        // Gets first iteration item
+        public override object First()
+        {
+            return _aggregate[0];
+        }
+
+        // Gets next iteration item
+        public override object Next()
+        {
+            object ret = null;
+            if (_current < _aggregate.Count - 1)
+            {
+                ret = _aggregate[++_current];
+            }
+
+            return ret;
+        }
+
+        // Gets current iteration item
+        public override object CurrentItem()
+        {
+            return _aggregate[_current];
+        }
+
+        // Gets whether iterations are complete
+        public override bool IsDone()
+        {
+            return _current >= _aggregate.Count;
+        }
+    }
