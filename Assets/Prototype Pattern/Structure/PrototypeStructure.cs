fdf7d7bd06ccdbb5f4e5411da2d89a26ee84e285
浅墨 <happylifemxy@163.com>
2016-10-12 11:53:00 +0800

    add 2 more pattern

---
+++ Assets/Prototype Pattern/Structure/PrototypeStructure.cs
@@ -0,0 +1,84 @@
+﻿//-------------------------------------------------------------------------------------
+//	PrototypeStructure.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class PrototypeStructure : MonoBehaviour
+{
+    /// <summary>
+    /// Entry point into console application.
+    /// </summary>
+    void Start( )
+    {
+        // Create two instances and clone each
+
+        ConcretePrototype1 p1 = new ConcretePrototype1("I");
+        ConcretePrototype1 c1 = (ConcretePrototype1)p1.Clone();
+        Debug.Log("Cloned: "+c1.Id);
+
+        ConcretePrototype2 p2 = new ConcretePrototype2("II");
+        ConcretePrototype2 c2 = (ConcretePrototype2)p2.Clone();
+        Debug.Log("Cloned: "+c2.Id);
+
+    }
+}
+
+/// <summary>
+/// The 'Prototype' abstract class
+/// </summary>
+abstract class Prototype
+{
+    private string _id;
+
+    // Constructor
+    public Prototype(string id)
+    {
+        this._id = id;
+    }
+
+    // Gets id
+    public string Id
+    {
+        get { return _id; }
+    }
+
+    public abstract Prototype Clone();
+}
+
+/// <summary>
+/// A 'ConcretePrototype' class 
+/// </summary>
+class ConcretePrototype1 : Prototype
+{
+    // Constructor
+    public ConcretePrototype1(string id)
+      : base(id)
+    {
+    }
+
+    // Returns a shallow copy
+    public override Prototype Clone()
+    {
+        return (Prototype)this.MemberwiseClone();
+    }
+}
+
+/// <summary>
+/// A 'ConcretePrototype' class 
+/// </summary>
+class ConcretePrototype2 : Prototype
+{
+    // Constructor
+    public ConcretePrototype2(string id)
+      : base(id)
+    {
+    }
+
+    // Returns a shallow copy
+    public override Prototype Clone()
+    {
+        return (Prototype)this.MemberwiseClone();
+    }
+}
---
+++ Assets/Singleton Pattern/Structure/SingletonStructure.cs
@@ -0,0 +1,51 @@
+﻿//-------------------------------------------------------------------------------------
+//	SingletonStructure.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SingletonStructure : MonoBehaviour
+{
+    /// <summary>
+    /// Entry point into console application.
+    /// </summary>
+    void Start()
+    {
+        // Constructor is protected -- cannot use new
+        Singleton s1 = Singleton.Instance();
+        Singleton s2 = Singleton.Instance();
+
+        // Test for same instance
+        if (s1 == s2)
+        {
+            Debug.Log("Objects are the same instance");
+        }
+    }
+}
+
+/// <summary>
+/// The 'Singleton' class
+/// </summary>
+class Singleton
+{
+    private static Singleton _instance;
+
+    // Constructor is 'protected'
+    protected Singleton()
+    {
+    }
+
+    public static Singleton Instance()
+    {
+        // Uses lazy initialization.
+        // Note: this is not thread safe.
+        if (_instance == null)
+        {
+            _instance = new Singleton();
+        }
+
+        return _instance;
+    }
+}
