be5a2f9b1d90b7c3f263105b6c36cd1513abe501
浅墨 <happylifemxy@163.com>
2016-10-12 21:51:40 +0800

    add proxy and Facade pattern

---
+++ Assets/Facade Pattern/Structure/FacadeStructure.cs
@@ -0,0 +1,95 @@
+﻿//-------------------------------------------------------------------------------------
+//	FacadeStructure.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class FacadeStructure : MonoBehaviour
+{
+	void Start ( )
+    {
+        Facade facade = new Facade();
+
+        facade.MethodA();
+        facade.MethodB();
+    }
+}
+
+/// <summary>
+/// The 'Subsystem ClassA' class
+/// </summary>
+class SubSystemOne
+{
+    public void MethodOne()
+    {
+        Debug.Log(" SubSystemOne Method");
+    }
+}
+
+/// <summary>
+/// The 'Subsystem ClassB' class
+/// </summary>
+class SubSystemTwo
+{
+    public void MethodTwo()
+    {
+        Debug.Log(" SubSystemTwo Method");
+    }
+}
+
+/// <summary>
+/// The 'Subsystem ClassC' class
+/// </summary>
+class SubSystemThree
+{
+    public void MethodThree()
+    {
+        Debug.Log(" SubSystemThree Method");
+    }
+}
+
+/// <summary>
+/// The 'Subsystem ClassD' class
+/// </summary>
+class SubSystemFour
+{
+    public void MethodFour()
+    {
+        Debug.Log(" SubSystemFour Method");
+    }
+}
+
+/// <summary>
+/// The 'Facade' class
+/// </summary>
+class Facade
+{
+    private SubSystemOne _one;
+    private SubSystemTwo _two;
+    private SubSystemThree _three;
+    private SubSystemFour _four;
+
+    public Facade()
+    {
+        _one = new SubSystemOne();
+        _two = new SubSystemTwo();
+        _three = new SubSystemThree();
+        _four = new SubSystemFour();
+    }
+
+    public void MethodA()
+    {
+        Debug.Log("\nMethodA() ---- ");
+        _one.MethodOne();
+        _two.MethodTwo();
+        _four.MethodFour();
+    }
+
+    public void MethodB()
+    {
+        Debug.Log("\nMethodB() ---- ");
+        _two.MethodTwo();
+        _three.MethodThree();
+    }
+}
---
+++ Assets/Proxy Pattern/Structure/ProxyStructure.cs
@@ -0,0 +1,58 @@
+﻿//-------------------------------------------------------------------------------------
+//	ProxyStructure.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace ProxyStructure
+{
+
+    public class ProxyStructure : MonoBehaviour
+    {
+	    void Start ( )
+        {
+            // Create proxy and request a service
+            Proxy proxy = new Proxy();
+            proxy.Request();
+        }
+    }
+
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    /// <summary>
+    /// The 'RealSubject' class
+    /// </summary>
+    class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Debug.Log("Called RealSubject.Request()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Proxy' class
+    /// </summary>
+    class Proxy : Subject
+    {
+        private RealSubject _realSubject;
+
+        public override void Request()
+        {
+            // Use 'lazy initialization'
+            if (_realSubject == null)
+            {
+                _realSubject = new RealSubject();
+            }
+
+            _realSubject.Request();
+        }
+    }
+}
