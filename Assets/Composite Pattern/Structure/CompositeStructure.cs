c77bcca6c1044330c7c81e8b3211e154ef80d6a7
浅墨 <happylifemxy@163.com>
2016-10-12 20:41:15 +0800

    add 2 new patterns

---
+++ Assets/Composite Pattern/Structure/CompositeStructure.cs
@@ -0,0 +1,115 @@
+﻿//-------------------------------------------------------------------------------------
+//	CompositeStructure.cs
+//-------------------------------------------------------------------------------------
+
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CompositeStructure : MonoBehaviour
+{
+	void Start ( )
+    {
+        // Create a tree structure
+        Composite root = new Composite("root");
+        root.Add(new Leaf("Leaf A"));
+        root.Add(new Leaf("Leaf B"));
+
+        Composite comp = new Composite("Composite X");
+        comp.Add(new Leaf("Leaf XA"));
+        comp.Add(new Leaf("Leaf XB"));
+
+        root.Add(comp);
+        root.Add(new Leaf("Leaf C"));
+
+        // Add and remove a leaf
+        Leaf leaf = new Leaf("Leaf D");
+        root.Add(leaf);
+        root.Remove(leaf);
+
+        // Recursively display tree
+        root.Display(1);
+
+    }
+}
+
+/// <summary>
+/// The 'Component' abstract class
+/// </summary>
+abstract class Component
+{
+    protected string name;
+
+    // Constructor
+    public Component(string name)
+    {
+        this.name = name;
+    }
+
+    public abstract void Add(Component c);
+    public abstract void Remove(Component c);
+    public abstract void Display(int depth);
+}
+
+/// <summary>
+/// The 'Composite' class
+/// </summary>
+class Composite : Component
+{
+    private List<Component> _children = new List<Component>();
+
+    // Constructor
+    public Composite(string name)
+      : base(name)
+    {
+    }
+
+    public override void Add(Component component)
+    {
+        _children.Add(component);
+    }
+
+    public override void Remove(Component component)
+    {
+        _children.Remove(component);
+    }
+
+    public override void Display(int depth)
+    {
+        Debug.Log(new String('-', depth) + name);
+
+        // Recursively display child nodes
+        foreach (Component component in _children)
+        {
+            component.Display(depth + 2);
+        }
+    }
+}
+
+/// <summary>
+/// The 'Leaf' class
+/// </summary>
+class Leaf : Component
+{
+    // Constructor
+    public Leaf(string name)
+      : base(name)
+    {
+    }
+
+    public override void Add(Component c)
+    {
+        Debug.Log("Cannot add to a leaf");
+    }
+
+    public override void Remove(Component c)
+    {
+        Debug.Log("Cannot remove from a leaf");
+    }
+
+    public override void Display(int depth)
+    {
+        Debug.Log(new String('-', depth) + name);
+    }
+}
---
+++ Assets/Decorator Pattern/Structure/DecoratorStructure.cs
@@ -0,0 +1,96 @@
+﻿//-------------------------------------------------------------------------------------
+//	DecoratorStructure.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace DecoratorStructure
+{
+    public class DecoratorStructure : MonoBehaviour
+    {
+	    void Start ( )
+        {
+            // Create ConcreteComponent and two Decorators
+            ConcreteComponent c = new ConcreteComponent();
+            ConcreteDecoratorA d1 = new ConcreteDecoratorA();
+            ConcreteDecoratorB d2 = new ConcreteDecoratorB();
+
+            // Link decorators
+            d1.SetComponent(c);
+            d2.SetComponent(d1);
+
+            d2.Operation();
+        }
+    }
+
+    /// <summary>
+    /// The 'Component' abstract class
+    /// </summary>
+    abstract class Component
+    {
+        public abstract void Operation();
+    }
+
+    /// <summary>
+    /// The 'ConcreteComponent' class
+    /// </summary>
+    class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Debug.Log("ConcreteComponent.Operation()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Decorator' abstract class
+    /// </summary>
+    abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteDecoratorA' class
+    /// </summary>
+    class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Debug.Log("ConcreteDecoratorA.Operation()");
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteDecoratorB' class
+    /// </summary>
+    class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+            Debug.Log("ConcreteDecoratorB.Operation()");
+        }
+
+        void AddedBehavior()
+        {
+        }
+    }
+
+}
