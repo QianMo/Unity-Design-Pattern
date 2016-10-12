b878b13df8ce9ffebddffd6713ea089d49302c4f
浅墨 <happylifemxy@163.com>
2016-10-12 18:03:48 +0800

    add Adapter and Bridge pattern

---
+++ Assets/Adapter Pattern/Structure/AdapterStructure.cs
@@ -0,0 +1,53 @@
+﻿//-------------------------------------------------------------------------------------
+//	AdapterStructure.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class AdapterStructure : MonoBehaviour
+{
+	void Start( )
+    {
+        // Create adapter and place a request
+        Target target = new Adapter();
+        target.Request();
+    }
+}
+
+/// <summary>
+/// The 'Target' class
+/// </summary>
+class Target
+{
+    public virtual void Request()
+    {
+        Debug.Log("Called Target Request()");
+    }
+}
+
+/// <summary>
+/// The 'Adapter' class
+/// </summary>
+class Adapter : Target
+{
+    private Adaptee _adaptee = new Adaptee();
+
+    public override void Request()
+    {
+        // Possibly do some other work
+        //  and then call SpecificRequest
+        _adaptee.SpecificRequest();
+    }
+}
+
+/// <summary>
+/// The 'Adaptee' class
+/// </summary>
+class Adaptee
+{
+    public void SpecificRequest()
+    {
+        Debug.Log("Called SpecificRequest()");
+    }
+}
---
+++ Assets/Bridge Pattern/Structure/BridgeStructure.cs
@@ -0,0 +1,82 @@
+﻿//-------------------------------------------------------------------------------------
+//	BridgeStructure.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class BridgeStructure : MonoBehaviour
+{
+	void Start ( )
+    {
+        Abstraction ab = new RefinedAbstraction();
+
+        // Set implementation and call
+        ab.Implementor = new ConcreteImplementorA();
+        ab.Operation();
+
+        // Change implemention and call
+        ab.Implementor = new ConcreteImplementorB();
+        ab.Operation();
+    }
+}
+
+/// <summary>
+/// The 'Abstraction' class
+/// </summary>
+class Abstraction
+{
+    protected Implementor implementor;
+
+    // Property
+    public Implementor Implementor
+    {
+        set { implementor = value; }
+    }
+
+    public virtual void Operation()
+    {
+        implementor.Operation();
+    }
+}
+
+/// <summary>
+/// The 'Implementor' abstract class
+/// </summary>
+abstract class Implementor
+{
+    public abstract void Operation();
+}
+
+/// <summary>
+/// The 'RefinedAbstraction' class
+/// </summary>
+class RefinedAbstraction : Abstraction
+{
+    public override void Operation()
+    {
+        implementor.Operation();
+    }
+}
+
+/// <summary>
+/// The 'ConcreteImplementorA' class
+/// </summary>
+class ConcreteImplementorA : Implementor
+{
+    public override void Operation()
+    {
+        Debug.Log("ConcreteImplementorA Operation");
+    }
+}
+
+/// <summary>
+/// The 'ConcreteImplementorB' class
+/// </summary>
+class ConcreteImplementorB : Implementor
+{
+    public override void Operation()
+    {
+        Debug.Log("ConcreteImplementorB Operation");
+    }
+}
