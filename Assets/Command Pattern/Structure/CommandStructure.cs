97d0746ac18bf391db6c0487f9a9fe2a9fff9ee0
浅墨 <happylifemxy@163.com>
2016-10-13 13:50:34 +0800

    add more pattern structures

---
+++ Assets/Command Pattern/Structure/CommandStructure.cs
@@ -0,0 +1,87 @@
+﻿//-------------------------------------------------------------------------------------
+//	CommandStructure.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace CommandStructure
+{
+    public class CommandStructure : MonoBehaviour
+    {
+	    void Start ( )
+        {
+            // Create receiver, command, and invoker
+            Receiver receiver = new Receiver();
+            Command command = new ConcreteCommand(receiver);
+            Invoker invoker = new Invoker();
+
+            // Set and execute command
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
+        }
+    }
+
+    /// <summary>
+    /// The 'Command' abstract class
+    /// </summary>
+    abstract class Command
+    {
+        protected Receiver receiver;
+
+        // Constructor
+        public Command(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+
+    /// <summary>
+    /// The 'ConcreteCommand' class
+    /// </summary>
+    class ConcreteCommand : Command
+    {
+        // Constructor
+        public ConcreteCommand(Receiver receiver) :
+          base(receiver)
+        {
+        }
+
+        public override void Execute()
+        {
+            receiver.Action();
+        }
+    }
+
+    /// <summary>
+    /// The 'Receiver' class
+    /// </summary>
+    class Receiver
+    {
+        public void Action()
+        {
+          Debug.Log("Called Receiver.Action()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Invoker' class
+    /// </summary>
+    class Invoker
+    {
+        private Command _command;
+
+        public void SetCommand(Command command)
+        {
+            this._command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            _command.Execute();
+        }
+    }
+
+}
---
+++ Assets/State Pattern/Structure/StateStructure.cs
@@ -0,0 +1,86 @@
+﻿//-------------------------------------------------------------------------------------
+//	StateStructure.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace StateStructure
+{
+
+    public class StateStructure : MonoBehaviour
+    {
+	    void Start ( )
+        {
+            // Setup context in a state
+            Context c = new Context(new ConcreteStateA());
+
+            // Issue requests, which toggles state
+            c.Request();
+            c.Request();
+            c.Request();
+            c.Request();
+        }
+    }
+
+    /// <summary>
+    /// The 'State' abstract class
+    /// </summary>
+    abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    /// <summary>
+    /// A 'ConcreteState' class
+    /// </summary>
+    class ConcreteStateA : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteState' class
+    /// </summary>
+    class ConcreteStateB : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateA();
+        }
+    }
+
+    /// <summary>
+    /// The 'Context' class
+    /// </summary>
+    class Context
+    {
+        private State _state;
+
+        // Constructor
+        public Context(State state)
+        {
+            this.State = state;
+        }
+
+        // Gets or sets the state
+        public State State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+              Debug.Log("State: " +_state.GetType().Name);
+            }
+        }
+
+        public void Request()
+        {
+            _state.Handle(this);
+        }
+    }
+
+}
