a8c4aba18b2a98ab9dceb8aea0652cc4bcc331af
浅墨 <happylifemxy@163.com>
2016-10-16 21:18:54 +0800

    add composite pattern example

---
+++ Assets/Structural Patterns/Composite Pattern/Exmaple1/CompositePatternExample1.cs
@@ -0,0 +1,128 @@
+﻿//-------------------------------------------------------------------------------------
+//	CompositePatternExample1.cs
+//-------------------------------------------------------------------------------------
+
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+//This real-world code demonstrates the Composite pattern used in building a graphical tree structure made up of primitive nodes(lines, circles, etc) and composite 
+// nodes(groups of drawing elements that make up more complex elements).
+
+namespace CompositePatternExample1
+{
+    public class CompositePatternExample1 : MonoBehaviour
+    {
+        void Start()
+        {
+            // Create a tree structure 
+            CompositeElement root =
+              new CompositeElement("Picture");
+            root.Add(new PrimitiveElement("Red Line"));
+            root.Add(new PrimitiveElement("Blue Circle"));
+            root.Add(new PrimitiveElement("Green Box"));
+
+            // Create a branch
+            CompositeElement comp =
+              new CompositeElement("Two Circles");
+            comp.Add(new PrimitiveElement("Black Circle"));
+            comp.Add(new PrimitiveElement("White Circle"));
+            root.Add(comp);
+
+            // Add and remove a PrimitiveElement
+            PrimitiveElement pe =
+              new PrimitiveElement("Yellow Line");
+            root.Add(pe);
+            root.Remove(pe);
+
+            // Recursively display nodes
+            root.Display(1);
+
+        }
+    }
+
+
+    /// <summary>
+    /// The 'Component' Treenode
+    /// </summary>
+    abstract class DrawingElement
+    {
+        protected string _name;
+
+        // Constructor
+        public DrawingElement(string name)
+        {
+            this._name = name;
+        }
+
+        public abstract void Add(DrawingElement d);
+        public abstract void Remove(DrawingElement d);
+        public abstract void Display(int indent);
+    }
+
+    /// <summary>
+    /// The 'Leaf' class
+    /// </summary>
+    class PrimitiveElement : DrawingElement
+    {
+        // Constructor
+        public PrimitiveElement(string name)
+          : base(name)
+        {
+        }
+
+        public override void Add(DrawingElement c)
+        {
+            Debug.Log("Cannot add to a PrimitiveElement");
+        }
+
+        public override void Remove(DrawingElement c)
+        {
+            Debug.Log("Cannot remove from a PrimitiveElement");
+        }
+
+        public override void Display(int indent)
+        {
+            Debug.Log(new String('-', indent) + " " + _name);
+        }
+    }
+
+    /// <summary>
+    /// The 'Composite' class
+    /// </summary>
+    class CompositeElement : DrawingElement
+    {
+        private List<DrawingElement> elements =
+          new List<DrawingElement>();
+
+        // Constructor
+        public CompositeElement(string name)
+          : base(name)
+        {
+        }
+
+        public override void Add(DrawingElement d)
+        {
+            elements.Add(d);
+        }
+
+        public override void Remove(DrawingElement d)
+        {
+            elements.Remove(d);
+        }
+
+        public override void Display(int indent)
+        {
+            Debug.Log(new String('-', indent) +
+              "+ " + _name);
+
+            // Display each child element on this node
+            foreach (DrawingElement d in elements)
+            {
+                d.Display(indent + 2);
+            }
+        }
+    }
+}
