28dd399357f581202d385015583c2efee16494ad
浅墨 <happylifemxy@163.com>
2016-10-16 13:22:17 +0800

    add bridge pattern example

---
+++ Assets/Structural Patterns/Bridge Pattern/Exmaple1/BridgePatternExample1.cs
@@ -0,0 +1,179 @@
+﻿//-------------------------------------------------------------------------------------
+//	BridgePatternExample1.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//This real-world code demonstrates the Bridge pattern in which a BusinessObject abstraction is decoupled from the implementation in DataObject.
+//The DataObject implementations can evolve dynamically without changing any clients.
+
+namespace BridgePatternExample1
+{
+
+    public class BridgePatternExample1 : MonoBehaviour
+    {
+        void Start()
+        {
+            // Create RefinedAbstraction
+            Customers customers = new Customers("Chicago");
+
+            // Set ConcreteImplementor
+            customers.Data = new CustomersData();
+
+            // Exercise the bridge
+            customers.Show();
+            customers.Next();
+            customers.Show();
+            customers.Next();
+            customers.Show();
+            customers.Add("Henry Velasquez");
+
+            customers.ShowAll();
+        }
+    }
+
+    /// <summary>
+    /// The 'Abstraction' class
+    /// </summary>
+    class CustomersBase
+    {
+        private DataObject _dataObject;
+        protected string group;
+
+        public CustomersBase(string group)
+        {
+            this.group = group;
+        }
+
+        // Property
+        public DataObject Data
+        {
+            set { _dataObject = value; }
+            get { return _dataObject; }
+        }
+
+        public virtual void Next()
+        {
+            _dataObject.NextRecord();
+        }
+
+        public virtual void Prior()
+        {
+            _dataObject.PriorRecord();
+        }
+
+        public virtual void Add(string customer)
+        {
+            _dataObject.AddRecord(customer);
+        }
+
+        public virtual void Delete(string customer)
+        {
+            _dataObject.DeleteRecord(customer);
+        }
+
+        public virtual void Show()
+        {
+            _dataObject.ShowRecord();
+        }
+
+        public virtual void ShowAll()
+        {
+            Debug.Log("Customer Group: " + group);
+            _dataObject.ShowAllRecords();
+        }
+    }
+
+    /// <summary>
+    /// The 'RefinedAbstraction' class
+    /// </summary>
+    class Customers : CustomersBase
+    {
+        // Constructor
+        public Customers(string group)
+          : base(group)
+        {
+        }
+
+        public override void ShowAll()
+        {
+            // Add separator lines
+            Debug.Log("------------------------");
+            base.ShowAll();
+            Debug.Log("------------------------");
+        }
+    }
+
+    /// <summary>
+    /// The 'Implementor' abstract class
+    /// </summary>
+    abstract class DataObject
+    {
+        public abstract void NextRecord();
+        public abstract void PriorRecord();
+        public abstract void AddRecord(string name);
+        public abstract void DeleteRecord(string name);
+        public abstract void ShowRecord();
+        public abstract void ShowAllRecords();
+    }
+
+    /// <summary>
+    /// The 'ConcreteImplementor' class
+    /// </summary>
+    class CustomersData : DataObject
+    {
+        private List<string> _customers = new List<string>();
+        private int _current = 0;
+
+        public CustomersData()
+        {
+            // Loaded from a database 
+            _customers.Add("Jim Jones");
+            _customers.Add("Samual Jackson");
+            _customers.Add("Allen Good");
+            _customers.Add("Ann Stills");
+            _customers.Add("Lisa Giolani");
+        }
+
+        public override void NextRecord()
+        {
+            if (_current <= _customers.Count - 1)
+            {
+                _current++;
+            }
+        }
+
+        public override void PriorRecord()
+        {
+            if (_current > 0)
+            {
+                _current--;
+            }
+        }
+
+        public override void AddRecord(string customer)
+        {
+            _customers.Add(customer);
+        }
+
+        public override void DeleteRecord(string customer)
+        {
+            _customers.Remove(customer);
+        }
+
+        public override void ShowRecord()
+        {
+            Debug.Log(_customers[_current]);
+        }
+
+        public override void ShowAllRecords()
+        {
+            foreach (string customer in _customers)
+            {
+                Debug.Log(" " + customer);
+            }
+        }
+    }
+}
