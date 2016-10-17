d04315c0a17f1c99a14fd6144703e3a2189fa8b4
浅墨 <happylifemxy@163.com>
2016-10-17 09:58:30 +0800

    Add Decorator Pattern Example

---
+++ Assets/Structural Patterns/Decorator Pattern/Exmaple1/DecoratorPatternExample1.cs
@@ -0,0 +1,162 @@
+﻿//-------------------------------------------------------------------------------------
+//	DecoratorPatternExample1.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//This real-world code demonstrates the Decorator pattern in which 'borrowable' functionality is added to existing library items(books and videos).
+
+namespace DecoratorPatternExample1
+{
+
+    public class DecoratorPatternExample1 : MonoBehaviour
+    {
+        void Start()
+        {
+            // Create book
+            Book book = new Book("Worley", "Inside ASP.NET", 10);
+            book.Display();
+
+            // Create video
+            Video video = new Video("Spielberg", "Jaws", 23, 92);
+            video.Display();
+
+            // Make video borrowable, then borrow and display
+            Debug.Log("\nMaking video borrowable:");
+
+            Borrowable borrowvideo = new Borrowable(video);
+            borrowvideo.BorrowItem("Customer #1");
+            borrowvideo.BorrowItem("Customer #2");
+
+            borrowvideo.Display();
+        }
+    }
+
+    /// <summary>
+    /// The 'Component' abstract class
+    /// </summary>
+    abstract class LibraryItem
+    {
+        private int _numCopies;
+
+        // Property
+        public int NumCopies
+        {
+            get { return _numCopies; }
+            set { _numCopies = value; }
+        }
+
+        public abstract void Display();
+    }
+
+    /// <summary>
+    /// The 'ConcreteComponent' class
+    /// </summary>
+    class Book : LibraryItem
+    {
+        private string _author;
+        private string _title;
+
+        // Constructor
+        public Book(string author, string title, int numCopies)
+        {
+            this._author = author;
+            this._title = title;
+            this.NumCopies = numCopies;
+        }
+
+        public override void Display()
+        {
+            Debug.Log("\nBook ------ ");
+            Debug.Log(" Author: "+ _author);
+            Debug.Log(" Title: "+ _title);
+            Debug.Log(" # Copies: "+ NumCopies);
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteComponent' class
+    /// </summary>
+    class Video : LibraryItem
+    {
+        private string _director;
+        private string _title;
+        private int _playTime;
+
+        // Constructor
+        public Video(string director, string title,
+          int numCopies, int playTime)
+        {
+            this._director = director;
+            this._title = title;
+            this.NumCopies = numCopies;
+            this._playTime = playTime;
+        }
+
+        public override void Display()
+        {
+            Debug.Log("\nVideo ----- ");
+            Debug.Log(" Director: "+ _director);
+            Debug.Log(" Title: "+ _title);
+            Debug.Log(" # Copies: "+ NumCopies);
+            Debug.Log(" Playtime: "+ _playTime+ "\n");
+        }
+    }
+
+    /// <summary>
+    /// The 'Decorator' abstract class
+    /// </summary>
+    abstract class Decorator : LibraryItem
+    {
+        protected LibraryItem libraryItem;
+
+        // Constructor
+        public Decorator(LibraryItem libraryItem)
+        {
+            this.libraryItem = libraryItem;
+        }
+
+        public override void Display()
+        {
+            libraryItem.Display();
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteDecorator' class
+    /// </summary>
+    class Borrowable : Decorator
+    {
+        protected List<string> borrowers = new List<string>();
+
+        // Constructor
+        public Borrowable(LibraryItem libraryItem)
+          : base(libraryItem)
+        {
+        }
+
+        public void BorrowItem(string name)
+        {
+            borrowers.Add(name);
+            libraryItem.NumCopies--;
+        }
+
+        public void ReturnItem(string name)
+        {
+            borrowers.Remove(name);
+            libraryItem.NumCopies++;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+
+            foreach (string borrower in borrowers)
+            {
+                Debug.Log(" borrower: " + borrower);
+            }
+        }
+    }
+}
