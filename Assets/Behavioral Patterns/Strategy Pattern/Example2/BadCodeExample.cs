ee09e638c755ab10e5a3bfd86e3e1485b65d9217
浅墨 <happylifemxy@163.com>
2016-11-06 21:04:15 +0800

    update strategy pattern example

---
+++ Assets/Behavioral Patterns/Strategy Pattern/Example2/BadCodeExample.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animal_BadExample
+{
+	// bad code:
+	public virtual void Fly()
+	{
+		Debug.Log ("Can Fly");
+	}
+}
+
+public class Dog_BadExample : Animal_BadExample
+{
+	// bad code:
+	public override void Fly()
+	{
+		// override
+	}
+}
+
+public class Cat_BadExample : Animal_BadExample
+{
+	// bad code:
+	public override void Fly()
+	{
+		// override
+	}
+}
+
+public class Bird_BadExample : Animal_BadExample
+{
+	public override void Fly()
+	{
+	}
+}
+
+// Rembember:
+// ALWAYS
+// eliminate duplicate code
+// eliminate technices that allow one class to affect others
---
+++ Assets/Behavioral Patterns/Strategy Pattern/Example2/StrategyPatternExample2.cs
@@ -0,0 +1,103 @@
+﻿//-------------------------------------------------------------------------------------
+//	StrategyPatternExample2.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace StrategyPatternExample2
+{
+    public class StrategyPatternExample2 : MonoBehaviour
+    {
+        void Start()
+        {
+            // create dog and bird example objects
+            Animal sparky = new Dog();
+            Animal tweety = new Bird();
+
+            Debug.Log("Dog: " + sparky.TryToFly());
+            Debug.Log("Bird: " + tweety.TryToFly());
+
+            // change behaviour of dog
+            sparky.SetFlyingAbility(new ItFlys());
+
+            Debug.Log("Dog: " + sparky.TryToFly());
+            Debug.Log("Bird: " + tweety.TryToFly());
+        }
+    }
+
+
+    // Using Interfaces for decoupling
+    // putting behaviour that varies in different classes
+    public interface IFly
+    {
+        string Fly();
+    }
+
+    // Class that holds behaviour for objects that can fly
+    class ItFlys : IFly
+    {
+        public string Fly()
+        {
+            return "Flying high";
+        }
+    }
+
+    // Class that holds behaviour for objects that can not fly
+    class CantFly : IFly
+    {
+        public string Fly()
+        {
+            return "I can't fly";
+        }
+    }
+
+    class FlyingSuperFast : IFly
+    {
+        public string Fly()
+        {
+            return "Fly super fast";
+        }
+    }
+
+
+    // Classes that hold an instance of the behaviours above:
+    public class Animal
+    {
+        // hereby adding the behaviour
+        // it also can change that way
+        public IFly flyingType;
+
+        public string TryToFly()
+        {
+            return flyingType.Fly();
+        }
+
+        public void SetFlyingAbility(IFly newFlyingType)
+        {
+            this.flyingType = newFlyingType;
+        }
+    }
+
+    // derived objects that vary from the base Animal:
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+            flyingType = new CantFly();
+        }
+    }
+
+    public class Bird : Animal
+    {
+        public Bird()
+        {
+            flyingType = new ItFlys();
+        }
+    }
+}
+
+// Rembember:
+// ALWAYS
+// eliminate duplicate code
+// eliminate technices that allow one class to affect others
