22b335913ff5400f601192c2a4720bc86b65839a
浅墨 <happylifemxy@163.com>
2017-01-10 20:23:47 +0800

    add 2 more example
    add 2 more example

---
+++ Assets/Behavioral Patterns/Visitor Pattern/Exmaple2/VisitorPatternExample2.cs
@@ -0,0 +1,183 @@
+﻿//-------------------------------------------------------------------------------------
+//	VisitorPatternExample2.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VisitorPatternExample2
+{
+    public class VisitorPatternExample2 : MonoBehaviour
+    {
+        void Start()
+        {
+            // Setup employee collection
+            Employees e = new Employees();
+            e.Attach(new Clerk());
+            e.Attach(new Director());
+            e.Attach(new President());
+
+            // Employees are 'visited'
+            e.Accept(new IncomeVisitor());
+            e.Accept(new VacationVisitor());
+        }
+
+
+        /// <summary>
+        /// The 'Visitor' interface
+        /// </summary>
+        interface IVisitor
+        {
+            void Visit(Element element);
+        }
+
+        /// <summary>
+        /// A 'ConcreteVisitor' class
+        /// </summary>
+        class IncomeVisitor : IVisitor
+        {
+            public void Visit(Element element)
+            {
+                Employee employee = element as Employee;
+
+                // Provide 10% pay raise
+                employee.Income *= 1.10;
+
+                Debug.Log(string.Format("{0} {1}'s new income: {2:C}",
+                  employee.GetType().Name, employee.Name,
+                  employee.Income));
+            }
+        }
+
+        /// <summary>
+        /// A 'ConcreteVisitor' class
+        /// </summary>
+        class VacationVisitor : IVisitor
+        {
+            public void Visit(Element element)
+            {
+                Employee employee = element as Employee;
+
+                // Provide 3 extra vacation days
+                employee.VacationDays += 3;
+                Debug.Log(string.Format("{0} {1}'s new vacation days: {2}",
+                  employee.GetType().Name, employee.Name,
+                  employee.VacationDays));
+            }
+        }
+
+        /// <summary>
+        /// The 'Element' abstract class
+        /// </summary>
+        abstract class Element
+        {
+            public abstract void Accept(IVisitor visitor);
+        }
+
+        /// <summary>
+        /// The 'ConcreteElement' class
+        /// </summary>
+        class Employee : Element
+        {
+            private string _name;
+            private double _income;
+            private int _vacationDays;
+
+            // Constructor
+            public Employee(string name, double income,
+              int vacationDays)
+            {
+                this._name = name;
+                this._income = income;
+                this._vacationDays = vacationDays;
+            }
+
+            // Gets or sets the name
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            // Gets or sets income
+            public double Income
+            {
+                get { return _income; }
+                set { _income = value; }
+            }
+
+            // Gets or sets number of vacation days
+            public int VacationDays
+            {
+                get { return _vacationDays; }
+                set { _vacationDays = value; }
+            }
+
+            public override void Accept(IVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+        }
+
+        /// <summary>
+        /// The 'ObjectStructure' class
+        /// </summary>
+        class Employees
+        {
+            private List<Employee> _employees = new List<Employee>();
+
+            public void Attach(Employee employee)
+            {
+                _employees.Add(employee);
+            }
+
+            public void Detach(Employee employee)
+            {
+                _employees.Remove(employee);
+            }
+
+            public void Accept(IVisitor visitor)
+            {
+                foreach (Employee e in _employees)
+                {
+                    e.Accept(visitor);
+                }
+            }
+        }
+
+        // Three employee types
+
+        class Clerk : Employee
+        {
+            // Constructor
+            public Clerk()
+              : base("Hank", 25000.0, 14)
+            {
+            }
+        }
+
+        class Director : Employee
+        {
+            // Constructor
+            public Director()
+              : base("Elly", 35000.0, 16)
+            {
+            }
+        }
+
+        class President : Employee
+        {
+            // Constructor
+            public President()
+              : base("Dick", 45000.0, 21)
+            {
+            }
+        }
+
+
+
+    }
+
+}
+
---
+++ Assets/Creational Patterns/Abstract Factory Pattern/Example2/AbstractFactoryPatternExample2.cs
@@ -0,0 +1,173 @@
+﻿//-------------------------------------------------------------------------------------
+//	AbstractFactoryPatternExample2.cs
+//-------------------------------------------------------------------------------------
+
+// allows a family of related objects without specifying the concrete class
+// when there are many objects that can be added or changed dynamically
+// you can model everything you can image and have those objects interact through common interfaces
+
+// negative: can get very complicated
+
+
+using UnityEngine;
+using System.Collections;
+
+namespace AbstractFactoryPatternExample2
+{
+
+    public class AbstractFactoryPatternExample2 : MonoBehaviour
+    {
+	    void Start ( )
+	    {
+            EnemyShipBuilding ufoBuilder = new UFOEnemyShipBuilding();
+            ufoBuilder.orderShip(ShipType.UFO);
+        }
+    }
+
+    public enum ShipType
+    {
+        UFO
+    }
+
+
+    public abstract class EnemyShipBuilding
+    {
+        // abstract order form:
+        protected abstract EnemyShip MakeEnemyShip(ShipType type);
+
+        public EnemyShip orderShip(ShipType type)
+        {
+            EnemyShip ship = MakeEnemyShip(type);
+
+            ship.MakeShip();
+            ship.DisplayShip();
+            ship.FollowHeroShip();
+            ship.Shoot();
+
+            return ship;
+        }
+    }
+
+    public class UFOEnemyShipBuilding : EnemyShipBuilding
+    {
+        // Make Ship varies per ship type...
+        protected override EnemyShip MakeEnemyShip(ShipType type)
+        {
+            EnemyShip ship = null;
+
+            if (type == ShipType.UFO)
+            {
+                IEnemyShipFactory factory = new UFOEnemyShipFactory();
+                ship = new UFOEnemyShip(factory);
+                ship.name = "UFO";
+            }
+
+            return ship;
+        }
+    }
+
+
+
+
+
+    public interface IEnemyShipFactory
+    {
+        IESWeapon AddESGun();
+        IESEngine AddESEngine();
+    }
+
+    public class UFOEnemyShipFactory : IEnemyShipFactory
+    {
+        // each factory can add different weapons and stuff
+        public IESWeapon AddESGun()
+        {
+            return new ESUFOGun();
+        }
+
+        public IESEngine AddESEngine()
+        {
+            return new ESUFOEngine();
+        }
+    }
+
+
+
+
+
+    public abstract class EnemyShip
+    {
+        public string name;
+        protected IESEngine engine;
+        protected IESWeapon weapon;
+
+        public abstract void MakeShip();
+
+        public void DisplayShip()
+        {
+            Debug.Log(name + " is on the screen.");
+        }
+
+        public void FollowHeroShip()
+        {
+            Debug.Log(name + " follows hero ship with " + engine.ToString());
+        }
+
+        public void Shoot()
+        {
+            Debug.Log(name + " shoots and does " + weapon.ToString());
+        }
+
+        public string ToString()
+        {
+            return "The " + name + " has a speed of " + engine.ToString() + " a firepower of " + weapon.ToString();
+        }
+    }
+
+    public class UFOEnemyShip : EnemyShip
+    {
+        IEnemyShipFactory factory;
+
+        public UFOEnemyShip(IEnemyShipFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public override void MakeShip()
+        {
+            Debug.Log("Making enemy ship " + name);
+            weapon = factory.AddESGun();
+            engine = factory.AddESEngine();
+        }
+    }
+
+
+
+    // possible Weapons to swap in and out
+    public interface IESWeapon
+    {
+        string ToString();
+    }
+
+    public interface IESEngine
+    {
+        string ToString();
+    }
+
+    public class ESUFOGun : IESWeapon
+    {
+        public string ToString()
+        {
+            return "20 damage";
+        }
+    }
+    public class ESUFOEngine : IESEngine
+    {
+        public string ToString()
+        {
+            return "1000 mph";
+        }
+    }
+
+
+}
+
