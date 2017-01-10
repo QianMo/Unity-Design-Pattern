39b44f6c1cb99045ad9d4eadc4a95f20d840e624
浅墨 <happylifemxy@163.com>
2017-01-10 20:58:39 +0800

    Prototype Pattern example2
    Prototype Pattern example2

---
+++ Assets/Creational Patterns/Prototype Pattern/Example2/PrototypePatternExample2.cs
@@ -0,0 +1,71 @@
+﻿//-------------------------------------------------------------------------------------
+//	PrototypePatternExample2.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+using System;
+
+namespace PrototypePatternExample2
+{
+    public class PrototypePatternExample2 : MonoBehaviour
+    {
+        void Start()
+        {
+            CloneFactory factory = new CloneFactory();
+
+            Sheep sally = new Sheep();
+
+            Sheep clonedSheep = (Sheep)factory.GetClone(sally);
+
+            Debug.Log("Sally: " + sally.ToString());
+            Debug.Log("Clone of Sally: " + clonedSheep.ToString());
+            Debug.Log("Sally Hash: " + sally.GetHashCode() + " - Cloned Sheep Hash: " + clonedSheep.GetHashCode());
+        }
+
+    }
+
+    public class CloneFactory
+    {
+        public IAnimal GetClone(IAnimal animalSample)
+        {
+            return (IAnimal)animalSample.Clone();
+        }
+    }
+
+    public interface IAnimal : ICloneable
+    {
+        object Clone();
+    }
+
+    public class Sheep : IAnimal
+    {
+        public Sheep()
+        {
+            Debug.Log("Made Sheep");
+        }
+
+        public object Clone()
+        {
+            Sheep sheep = null;
+
+            try
+            {
+                sheep = (Sheep)base.MemberwiseClone();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error cloning Sheep");
+            }
+
+            return sheep;
+        }
+
+        public string ToString()
+        {
+            return "Hello I'm a Sheep";
+        }
+    }
+
+}
+
