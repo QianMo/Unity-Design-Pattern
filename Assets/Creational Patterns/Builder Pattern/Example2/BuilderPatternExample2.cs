6db7da53a789eb146c26afa05ac0936944683754
浅墨 <happylifemxy@163.com>
2017-01-10 20:48:45 +0800

    add 2 more pattern
    add 2 more pattern

---
+++ Assets/Creational Patterns/Builder Pattern/Example2/BuilderPatternExample2.cs
@@ -0,0 +1,144 @@
+﻿//-------------------------------------------------------------------------------------
+//	BuilderPatternExample2.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace BuilderPatternExample2
+{
+    public class BuilderPatternExample2 : MonoBehaviour
+    {
+        void Start()
+        {
+            IRobotBuilder oldRobot = new OldRobotBuilder();
+            RobotEngineer engineer = new RobotEngineer(oldRobot);
+            engineer.MakeRobot();
+
+            Robot firstRobot = engineer.GetRobot();
+            Debug.Log("First Robot built");
+            Debug.Log(firstRobot.ToString());
+        }
+    }
+
+    public interface IRobotPlan
+    {
+        void SetRobotHead(string head);
+
+        void SetRobotTorso(string torso);
+
+        void SetRobotArms(string arms);
+
+        void SetRobotLegs(string legs);
+    }
+
+    public class Robot : IRobotPlan
+    {
+        public string head { get; protected set; }
+        public string torso { get; protected set; }
+        public string arms { get; protected set; }
+        public string legs { get; protected set; }
+
+        public void SetRobotHead(string head)
+        {
+            this.head = head;
+        }
+
+        public void SetRobotTorso(string torso)
+        {
+            this.torso = torso;
+        }
+
+        public void SetRobotArms(string arms)
+        {
+            this.arms = arms;
+        }
+
+        public void SetRobotLegs(string legs)
+        {
+            this.legs = legs;
+        }
+
+        public string ToString()
+        {
+            return "Head: " + this.head + ", torso: " + this.torso + ", Arms: " + arms + ", legs: " + legs;
+        }
+    }
+
+
+
+
+    // they're kinda like a blueprint these RobotBuilder classes:
+    public interface IRobotBuilder
+    {
+        Robot GetRobot();
+        void BuildRobotHead();
+        void BuildRobotTorso();
+        void BuildRobotArms();
+        void BuildRobotLegs();
+    }
+
+    // for each new robot that you might want to have just create a new RobotBuilder Object
+    public class OldRobotBuilder : IRobotBuilder
+    {
+        protected Robot robot { get; set; }
+
+        public OldRobotBuilder()
+        {
+            this.robot = new Robot();
+        }
+
+        public Robot GetRobot()
+        {
+            return robot;
+        }
+
+        public void BuildRobotHead()
+        {
+            this.robot.SetRobotHead("Old Head");
+        }
+
+        public void BuildRobotTorso()
+        {
+            this.robot.SetRobotTorso("Old Torso");
+        }
+
+        public void BuildRobotArms()
+        {
+            this.robot.SetRobotArms("Old Arms");
+        }
+
+        public void BuildRobotLegs()
+        {
+            this.robot.SetRobotLegs("Roller Skates");
+        }
+    }
+
+
+
+    // he just calls the method in the Robot Objects (which are defined by the interface, just think of blueprints)
+    public class RobotEngineer
+    {
+        public IRobotBuilder robotBuilder { get; protected set; }
+
+        public RobotEngineer(IRobotBuilder builder)
+        {
+            this.robotBuilder = builder;
+        }
+
+        public Robot GetRobot()
+        {
+            return this.robotBuilder.GetRobot();
+        }
+
+        public void MakeRobot()
+        {
+            this.robotBuilder.BuildRobotHead();
+            this.robotBuilder.BuildRobotTorso();
+            this.robotBuilder.BuildRobotArms();
+            this.robotBuilder.BuildRobotLegs();
+        }
+    }
+
+
+}
---
+++ Assets/Creational Patterns/Factory Method Pattern/Example2/FactoryMethodPatternExample2.cs
@@ -0,0 +1,156 @@
+﻿//-------------------------------------------------------------------------------------
+//	FactoryMethodPatternExample2.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace FactoryMethodPatternExample2
+{
+    public class FactoryMethodPatternExample2 : MonoBehaviour
+    {
+        EnemyShipFactory factory;
+        void Start()
+        {
+            factory = new EnemyShipFactory();
+            Debug.Log("please type U、R、B for test~");
+        }
+
+        void Update( )
+        {
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                EnemyShip ship = factory.MakeEnemyShip(EnemyShipFactory.ShipType.UFO);
+                DoStuffEnemy(ship);
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                EnemyShip ship = factory.MakeEnemyShip(EnemyShipFactory.ShipType.Rocket);
+                DoStuffEnemy(ship);
+            }
+            else if (Input.GetKeyDown(KeyCode.B))
+            {
+                EnemyShip ship = factory.MakeEnemyShip(EnemyShipFactory.ShipType.Endboss);
+                DoStuffEnemy(ship);
+            }
+        }
+
+        void DoStuffEnemy(EnemyShip enemyShip)
+        {
+            enemyShip.displayEnemyShip();
+            enemyShip.followHeroShip();
+            enemyShip.enemyShipShoots();
+        }
+
+    }
+
+    // move that desicion making into another class:
+    public class EnemyShipFactory
+    {
+        public enum ShipType
+        {
+            UFO,
+            Rocket,
+            Endboss
+        }
+        ;
+
+        public EnemyShip MakeEnemyShip(ShipType shipType)
+        {
+            if (shipType == ShipType.UFO)
+                return new UFOEnemyShip();
+            else if (shipType == ShipType.Rocket)
+                return new RocketEnemyShip();
+            else if (shipType == ShipType.Endboss)
+                return new BigEnemyShip();
+            else
+                return null;
+        }
+    }
+
+    public class BadCodeExample : MonoBehaviour
+    {
+        void Start()
+        {
+            EnemyShip ufo = new UFOEnemyShip();
+            Debug.Log("What kind of ship do you want? (R / U)");
+        }
+
+        // yes its dynamic but it is also modifyable
+        // you don't want this sort of stuff going on inside of your actual program
+        // lots of if and else statements and logic
+        void Update()
+        {
+            return; // < "deactivating"
+
+
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                EnemyShip ship = new UFOEnemyShip();
+                DoStuffEnemy(ship);
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                EnemyShip ship = new RocketEnemyShip();
+                DoStuffEnemy(ship);
+            }
+        }
+
+        void DoStuffEnemy(EnemyShip enemyShip)
+        {
+            enemyShip.displayEnemyShip();
+            enemyShip.followHeroShip();
+            enemyShip.enemyShipShoots();
+        }
+    }
+
+    public abstract class EnemyShip
+    {
+        public string name;
+        public double damage;
+
+        public void followHeroShip()
+        {
+            Debug.Log(name + " is following the hero.");
+        }
+
+        public void displayEnemyShip()
+        {
+            Debug.Log(name + " is on the screen.");
+        }
+
+        public void enemyShipShoots()
+        {
+            Debug.Log(name + " attacks and does " + damage + " damage");
+        }
+    }
+
+    public class UFOEnemyShip : EnemyShip
+    {
+        public UFOEnemyShip()
+        {
+            name = "UFOEnemyShip";
+            damage = 20;
+        }
+    }
+
+    public class RocketEnemyShip : EnemyShip
+    {
+        public RocketEnemyShip()
+        {
+            name = "RocketEnemyShip";
+            damage = 10;
+        }
+    }
+
+    public class BigEnemyShip : UFOEnemyShip
+    {
+        public BigEnemyShip()
+        {
+            name = "BigEnemyShip";
+            damage = 40;
+        }
+    }
+
+}
+
