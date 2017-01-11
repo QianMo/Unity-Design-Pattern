bcf69a01c1972ed09e4bb59e6da059a786d46389
浅墨 <happylifemxy@163.com>
2017-01-11 09:39:12 +0800

    add more example

---
+++ Assets/Structural Patterns/Decorator Pattern/Exmaple2/DecoratorPatternExample2.cs
@@ -0,0 +1,141 @@
+﻿//-------------------------------------------------------------------------------------
+//	DecoratorPatternExample2.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace DecoratorPatternExample2
+{
+    public class DecoratorPatternExample2 : MonoBehaviour
+    {
+        void Start()
+        {
+            // Make Pizzas:
+            IPizza basicPizza = new TomatoSauce(new Mozzarella(new PlainPizza()));
+            Debug.Log("Ingredients of Pizza: " + basicPizza.GetDescription());
+            Debug.Log("Total Cost: " + basicPizza.GetCost());
+        }
+    }
+
+
+
+    public interface IPizza
+    {
+        string GetDescription();
+        double GetCost();
+    }
+
+
+    public class PlainPizza : IPizza
+    {
+        public string GetDescription()
+        {
+            return "Thin Dough";
+        }
+
+        public double GetCost()
+        {
+            return 4.00;
+        }
+    }
+
+
+
+    public abstract class ToppingDecorator : IPizza
+    {
+        protected IPizza tempPizza;
+
+        public ToppingDecorator(IPizza newPizza)
+        {
+            this.tempPizza = newPizza;
+        }
+
+
+        public virtual string GetDescription()
+        {
+            return tempPizza.GetDescription();
+        }
+
+        public virtual double GetCost()
+        {
+            return tempPizza.GetCost();
+        }
+    }
+
+
+
+    public class Mozzarella : ToppingDecorator
+    {
+        public Mozzarella(IPizza newPizza) : base(newPizza)
+        {
+            Debug.Log("Adding Dough");
+            Debug.Log("Adding Morarella");
+        }
+
+        public override string GetDescription()
+        {
+            return tempPizza.GetDescription() + ", Mozzarella";
+        }
+
+        public override double GetCost()
+        {
+            return tempPizza.GetCost() + 0.50;
+        }
+    }
+
+
+    public class TomatoSauce : ToppingDecorator
+    {
+        public TomatoSauce(IPizza newPizza) : base(newPizza)
+        {
+            Debug.Log("Adding Sauce");
+        }
+
+        public override string GetDescription()
+        {
+            return tempPizza.GetDescription() + ", Tomato Sauce";
+        }
+
+        public override double GetCost()
+        {
+            return tempPizza.GetCost() + 0.35;
+        }
+    }
+
+
+
+
+    namespace BadStyleExample
+    {
+        public abstract class Pizza
+        {
+            public abstract void SetDescription(string newDescription);
+            public abstract string GetDescription();
+            public abstract double GetCost();
+            //public abstract bool HasFontina();
+        }
+
+        // this way would force to create an infinite amount of subclasses for each type of pizza
+        // and if cost if calculated off of individual topings you would have to change cost for all pizzas
+        // if cost for one topping chages
+        public class ThreeCheesePizza : Pizza
+        {
+            public override void SetDescription(string newDescription)
+            {
+            }
+
+            public override string GetDescription()
+            {
+                return "Mozarella, Fontina, Parmesan, Cheese Pizza";
+            }
+
+            public override double GetCost()
+            {
+                return 10.00;
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Structural Patterns/Facade Pattern/Exmaple2/FacadePatternExample2.cs
@@ -0,0 +1,177 @@
+﻿//-------------------------------------------------------------------------------------
+//	FacadePatternExample2.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace FacadePatternExample2
+{
+    public class FacadePatternExample2 : MonoBehaviour
+    {
+        void Start()
+        {
+            BankAccountFacade bankAccount = new BankAccountFacade(12345678, 1234);
+
+            Debug.Log("\"I want to withdraw $50 dollars!\"");
+            bankAccount.WithdrawCash(50.00);
+
+            Debug.Log("\"Mh ok now let me withdraw $5000 dollars!?\"");
+            bankAccount.WithdrawCash(5000.00);
+
+            Debug.Log("\"What tf... maybe save some cash, here are $300 bucks ;-)\"");
+            bankAccount.DepositCash(300.00);
+        }
+    }
+
+    public class WelcomeToBank
+    {
+        public WelcomeToBank()
+        {
+            Debug.Log("Welcome to ABC Bank");
+            Debug.Log("We are happy to deposit your money :-)");
+        }
+    }
+
+    public class AccountNumberCheck
+    {
+        private int accountNumber = 12345678;
+
+        public int GetAccountNumber()
+        {
+            return accountNumber;
+        }
+
+        public bool AccountActive(int accNumber)
+        {
+            if (accNumber == accountNumber)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class SecurityCodeCheck
+    {
+        private int securityCode = 1234;
+
+        public int GetSecurityCode()
+        {
+            return securityCode;
+        }
+
+        public bool IsCodeCorrect(int code)
+        {
+            if (code == securityCode)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class FundsCheck
+    {
+        private double cashInAccount = 1000.00;
+
+        public double GetCashInAccount()
+        {
+            return cashInAccount;
+        }
+
+        protected void DecreaseCashInAccount(double cash)
+        {
+            cashInAccount -= cash;
+        }
+
+        protected void IncreaseCashInAccount(double cash)
+        {
+            cashInAccount += cash;
+        }
+
+        public bool HaveEnoughMoney(double cashToWithdraw)
+        {
+            if (cashToWithdraw > GetCashInAccount())
+            {
+                Debug.Log("You don't have enouth money.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void MakeDeposit(double cash)
+        {
+            IncreaseCashInAccount(cash);
+            Debug.Log("Deposit complete. Current Balance is: " + GetCashInAccount());
+        }
+
+        public void WithdrawMoney(double cash)
+        {
+            DecreaseCashInAccount(cash);
+            Debug.Log("Withdraw complete. Current Balance is: " + GetCashInAccount());
+        }
+    }
+
+    public class BankAccountFacade
+    {
+        private int accountNumber;
+        private int securityCode;
+        AccountNumberCheck accChecker;
+        SecurityCodeCheck codeChecker;
+        FundsCheck fundChecker;
+        WelcomeToBank bankWelcome;
+
+        public BankAccountFacade(int accountNumber, int newSecurityCode)
+        {
+            this.accountNumber = accountNumber;
+            this.securityCode = newSecurityCode;
+
+            bankWelcome = new WelcomeToBank();
+            codeChecker = new SecurityCodeCheck();
+            accChecker = new AccountNumberCheck();
+            fundChecker = new FundsCheck();
+        }
+
+        public int GetAccountNumber()
+        {
+            return accountNumber;
+        }
+
+        public int GetSecurityCode()
+        {
+            return securityCode;
+        }
+
+        public void WithdrawCash(double cash)
+        {
+            if (accChecker.AccountActive(GetAccountNumber())
+                && codeChecker.IsCodeCorrect(GetSecurityCode())
+                && fundChecker.HaveEnoughMoney(cash))
+            {
+                fundChecker.WithdrawMoney(cash);
+                Debug.Log("Transaction complete.");
+            }
+            else
+            {
+                Debug.Log("Transaction failed.");
+            }
+        }
+
+        public void DepositCash(double cash)
+        {
+            if (accChecker.AccountActive(GetAccountNumber())
+                && codeChecker.IsCodeCorrect(GetSecurityCode()))
+            {
+                fundChecker.MakeDeposit(cash);
+                Debug.Log("Transaction complete.");
+            }
+            else
+            {
+                Debug.Log("Transaction failed.");
+            }
+        }
+    }
+
+}
