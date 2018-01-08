3d7e24f9cd887b0e9cae557bf0d528d4510ff7a6
浅墨 <happylifemxy@163.com>
2018-01-08 20:12:59 +0800

    给单例模式加上更直观的一个示例
    给单例模式加上更直观的一个示例

---
+++ Assets/Creational Patterns/Singleton Pattern/Example2/SingletonPatternExample2.cs
@@ -0,0 +1,57 @@
+﻿//-------------------------------------------------------------------------------------
+//	SingletonPatternExample2.cs
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace SingletonPatternExample2
+{
+    public class SingletonPatternExample2 : MonoBehaviour
+    {
+        void Start()
+        {
+            RenderManager.Instance.Show();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                RenderManager.Instance.Show();
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 某单例manager
+    /// </summary>
+    public class RenderManager
+    {
+        private static RenderManager instance;
+        public static RenderManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new RenderManager();
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// 随便某方法
+        /// </summary>
+        public void Show()
+        {
+            Debug.Log("RenderManager is a Singleton !");
+        }
+    }
+
+
+
+
+}
